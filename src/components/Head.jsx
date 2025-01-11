ee773023a72550457578356bc88c3b9b26ed8eb8
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-01-11 18:10:22 -0500

    feat(Head): animacion

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -5,21 +5,26 @@ const Head = () => {
   return (
     <div className="container_head flex_row box">
       <div className="recuadro flex_column">
-        <div className="date_hr">
-          <hr aria-orientation="horizontal" className="hr_line line_position" />
-          <span className="date">15.02.2025</span>
+      <div data-aos="fade-up-left">
+          <div className="date_hr">
+            <hr
+              aria-orientation="horizontal"
+              className="hr_line line_position"
+            />
+            <span className="date">15.02.2025</span>
+          </div>
+          <h1 className="names_couple flex_row">
+            <span>
+              Cristian <br />&<br />
+              Mariapaz
+            </span>
+          </h1>
+          <hr aria-orientation="horizontal" className="hr_line line_bottom" />
+          <p className="text_head text_secondary_p">
+            Llenos de gratitud, hemos encontrado en Dios y en el amor mutuo, la
+            fuerza para dar este paso. Te invitamos a acompañarnos en este día.
+          </p>
         </div>
-        <h1 className="names_couple flex_row">
-          <span>
-            Cristian <br />&<br />
-            Mariapaz
-          </span>
-        </h1>
-        <hr aria-orientation="horizontal" className="hr_line line_bottom" />
-        <p className="text_head text_secondary_p">
-          Llenos de gratitud, hemos encontrado en Dios y en el amor mutuo, la
-          fuerza para dar este paso. Te invitamos a acompañarnos en este día.
-        </p>
       </div>
     </div>
   );
